eda7c3f975dc6879d99d8d7dfe9785b525be6405
Nick <nandreala@gmail.com>
2022-02-19 01:54:49 -0500

    initial commit

---
+++ components/Memory.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Memory() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <p>MAKE THE JUMP TO HYPERSPEED</p>
+      <h1>CORSAIR MEMORY</h1>
+      <p>CORSAIR has always led the way in high-performance DRAM.</p>
+      <p>
+        Now we&apos;ve made the leap to the next level of performance with DDR5.
+      </p>
+    </div>
+  );
+}
+
+export default Memory;
---
+++ components/NavBar.js
@@ -0,0 +1,35 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+function NavBar() {
+  const panel1 = useRef();
+  const panel2 = useRef();
+  const panel3 = useRef();
+  const panel4 = useRef();
+  const panel5 = useRef();
+  const panels = { 1: panel1, 2: panel2, 3: panel3, 4: panel4, 5: panel5 };
+  const [active, setActive] = useState(panel1);
+  //   useEffect(() => {
+  //     console.log(active);
+  //   }, [active]);
+
+  return (
+    <div className="flex items-center justify-center h-24 bg-black">
+      {Object.keys(panels).map((key) => (
+        <h3
+          key={key}
+          ref={panels[key]}
+          onClick={() => setActive(panels[key])}
+          className={`mx-5  box-border text-2xl font-bold ${
+            active === panels[key]
+              ? 'text-stone-50 border-b-4 border-b-yellow-300'
+              : 'text-stone-400'
+          } border-b-4 border-b-transparent hover: cursor-pointer`}
+        >
+          PANEL {key}
+        </h3>
+      ))}
+    </div>
+  );
+}
+
+export default NavBar;
